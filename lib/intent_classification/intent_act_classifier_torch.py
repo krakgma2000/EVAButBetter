c890a787eed5a120c13d43713f22e880286e19d9
Igor <igoranchikkuzmin@yandex.ru>
2023-02-06 16:16:53 +0300

    feat(feat/intent_act): bert model for classification of dialog act added

--- lib/intent_classification/intent_act_classifier_torch.py
+++ lib/intent_classification/intent_act_classifier_torch.py
@@ -1,1 +1,9 @@
 from lib.intent_classification.intent_classifier import Classifier
+
+class ClassifierMRDA(Classifier):
+    # def __init__(self, model:):
+    #     super().__init__()
+    #     self.model = model
+    #
+    # def predict(self, embedding):
+    #     self.model.predict(embedding)
---
+++ lib/neural_networks/bert_classifier.py
@@ -0,0 +1,71 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+MODEL_NAME = 'huawei-noah/TinyBERT_General_4L_312D'
+LABELS = ['fh', 's', 'b', '%', 'qy', 'fg', 'qw', 'qrr', 'h', 'qr', 'qo', 'qh']
+model_bert = AutoModel.from_pretrained(MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+PATH = '../models/mrda_bert_312_10ep.pth'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class BERTModel(torch.nn.Module):
+    def __init__(self, model, output_size):
+        super(BERTModel, self).__init__()
+        self.bert = model  # transformers.BertModel.from_pretrained(model_name)#huawei-noah/TinyBERT_General_6L_768D
+        self.dropout1 = torch.nn.Dropout(0.3)
+        self.l2 = torch.nn.Linear(self.bert.config.hidden_size, output_size)
+        self.relu1 = torch.nn.ReLU()
+
+    def forward(self, input_ids, attention_mask):
+        _, output = self.bert(input_ids, attention_mask=attention_mask, return_dict=False)
+
+        output = self.dropout1(output)
+        output = self.l2(output)
+        output = self.relu1(output)
+        return output
+
+
+# MODEL_NAME = 'huawei-noah/TinyBERT_General_6L_768D'#'huawei-noah/TinyBERT_General_4L_312D'#huawei-noah/TinyBERT_General_6L_768D
+
+model = BERTModel(model=model_bert, output_size=len(LABELS))
+
+
+class BERTClassifier:
+    def __init__(self, model=model, tokenizer=tokenizer, model_path=PATH, labels=LABELS):
+        self.model = model
+        self.model.load_state_dict(torch.load(model_path, map_location=device))
+        self.model.to(device)
+        self.tokenizer = tokenizer
+        self.labels = labels
+
+    def predict(self, text, return_type='label'):
+        """ Calculate dialog act type of a text. `return_type` can be 'label' or 'proba' """
+        with torch.no_grad():
+            inputs = self.tokenizer(text, padding='max_length',
+                                    max_length=256, truncation=True, add_special_tokens=True,
+                                    pad_to_max_length=True, return_tensors="pt", )
+            ids = inputs.input_ids
+            ids = ids.to(device)
+            mask = inputs.attention_mask
+            mask = mask.to(device)
+            # token_type_ids = inputs.token_type_ids
+            # token_type_ids = token_type_ids.to(device)
+            outputs = self.model(ids, mask)
+            proba = torch.sigmoid(outputs).cpu().numpy()[0]
+
+        if return_type == 'label':
+            return self.labels[proba.argmax()]
+        elif return_type == 'index':
+            return proba.argmax()
+        elif return_type == 'proba':
+            max(proba)
+        return max(proba)
+
+
+# import time
+#
+# bert = BERTClassifier()
+# time_start = time.time()
+# print(bert.predict("Where are my pants?"))
+# print(time.time()-time_start)
