b3a4a674824905a480c18283c053647de933e9bf
Dongjun Tang <ttss501.g@gmail.com>
2023-02-07 17:04:41 +0100

    added professor's name

--- actions/actions.py
+++ actions/actions.py
@@ -42,13 +42,12 @@ def findFromCsv(tracker, slot_name, look_for):
     slot_value = tracker.get_slot(slot_name).lower()
     df["isIncluded"] = df[look_for].str.lower().str.find(slot_value)
     res = df.loc[df["isIncluded"] != -1]
-    # print(res)
-    
+    print(res)
     return res
-class ActionDepartInfo(Action):
+class ActionProfessorInfo(Action):
 
     def name(self) -> Text:
-        return "action_depart_info"
+        return "action_professor_info"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -58,6 +57,42 @@ class ActionDepartInfo(Action):
         msg = "Professor " + res["NOMBRE"].values[0]  + " is in " + res["GRUPO"].values[0] + " and " + res["NOMBRE"].values[0] + "'s office room number is " + res["DESPACHO"].values[0]
         dispatcher.utter_message(text=msg)
         print(tracker.slots)
+        # wav = tts.tts("hello world", speaker=tts.speakers[0], language=tts.languages[0])
+        # tts.tts_to_file(text = "hello world", speaker=tts.speakers[0], language=tts.languages[0], file_path = "output.wav")
+        return []
+
+class ActionDepartmentInfo(Action):
+
+    def name(self) -> Text:
+        return "action_department_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        res = findFromCsv(tracker, "ORG", "GRUPO")
+        # print(res)
+        msg = "There are  " + str(res.shape[0])  + " professors in " + res["GRUPO"].values[0]
+        dispatcher.utter_message(text=msg)
+        # print(tracker.slots)
+        # wav = tts.tts("hello world", speaker=tts.speakers[0], language=tts.languages[0])
+        # tts.tts_to_file(text = "hello world", speaker=tts.speakers[0], language=tts.languages[0], file_path = "output.wav")
+        return []
+class ActionFacultyInfo(Action):
+
+    def name(self) -> Text:
+        return "action_faculty_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # res = findFromCsv(tracker, "ORG", "GRUPO")
+        # # print(res)
+        # msg = "There are  " + str(res.shape[0])  + " professors in " + res["GRUPO"].values[0]
+        msg = "There are xx departments and xx professors in UPF"
+        dispatcher.utter_message(text=msg)
+        # print(tracker.slots)
         # wav = tts.tts("hello world", speaker=tts.speakers[0], language=tts.languages[0])
         # tts.tts_to_file(text = "hello world", speaker=tts.speakers[0], language=tts.languages[0], file_path = "output.wav")
         return []
